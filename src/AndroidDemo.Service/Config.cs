b121cfef698c5f83563608bacb9747c075602e80
Michael <vaiyanzi@gmail.com>
2016-02-18 13:36:27 +0800

    updated the url.

--- src/AndroidDemo.Service/Config.cs
+++ src/AndroidDemo.Service/Config.cs
@@ -2,7 +2,7 @@ namespace AndroidDemo.Service
 {
     public static class Config
     {
-        private const string BaseUrl = "http://192.168.1.153:8090/";
+        private const string BaseUrl = "http://192.168.1.53:8090/";
 
         public const string MoviesUrl = BaseUrl + "api/movies";
     }
